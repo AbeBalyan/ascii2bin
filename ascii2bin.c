c9139fa0f70cffb141146cb1e01afa7f27458199
Abraham Balyan <ab606450@ssh.sandbox.csun.edu>
2021-03-10 14:34:44 -0800

    Initial version

---
+++ ascii2bin.c
@@ -0,0 +1,26 @@
+/****************************/
+/* Author: Abraham Balyan   */
+/* Project: Ascii           */
+/* Date: 3/10/2021          */
+
+#include <stdio.h>
+#include <unistd.h> 
+
+int main()
+{
+   int offset = 48;  // ascii value of '0'
+   int number = 0;
+   char ascii_value;
+ 
+   int retval = read(0, &ascii_value, 1);
+   while ((retval == 1) && (ascii_value != '\n')) {
+      int digit = ascii_value - offset;
+      if ((digit == 1) || (digit == 0))
+         number = (number << 1) + digit; 
+      retval = read(0, &ascii_value, 1);
+   }
+   printf("%d\n", number);
+
+   return 0;
+}
+    
